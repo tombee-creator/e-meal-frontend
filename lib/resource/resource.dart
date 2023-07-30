c7753d73ec5202f5796596a0aed96be8cb15ec68
tombee-creator <tomomail1997@gmail.c.om>
2023-07-30 18:26:54 +0900

    リソースを追加しました。

--- lib/resource/resource.dart
+++ lib/resource/resource.dart
@@ -7,5 +7,8 @@ class Resource {
       "https://images.unsplash.com/photo-1570025533377-7c6fffd66a7a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2534&q=80";
 
   String get loginLabel => "LOG IN";
-  String get loginHelperMessage => "Googleアカウントのユーザ名、パスワードを入力してください。";
+  String get loginHelperMessage => "ユーザ名、パスワードを入力してください。";
+
+  String get signupLabel => "SIGN UP";
+  String get signupHelperMessage => "ユーザ名、パスワードを入力してください。";
 }
