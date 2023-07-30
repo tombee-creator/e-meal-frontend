de982f87b27710c36ea659580fb6bbe75c34a332
tombee-creator <tomomail1997@gmail.c.om>
2023-07-30 09:06:03 +0900

    ルートを追加しました。

--- lib/routes/route_generator.dart
+++ lib/routes/route_generator.dart
@@ -1,20 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/views/login.dart';
 
 import '../views/my_home_page.dart';
 
+/// flutterアプリのルートを作成するクラス
 class RouteGenerator {
+  RouteGenerator? _instance;
+
+  factory() => _instance ??= RouteGenerator();
+
   // 各ページを定義
   static const String home = '/';
-  static const String hoge = '/hoge';
+  static const String login = '/login';
 
-  static Route<dynamic> generateRoute(RouteSettings settings) {
+  Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case home:
         return MaterialPageRoute(
           builder: (_) => const MyHomePage(title: "つみたべ"),
         );
+      case login:
+        return MaterialPageRoute(
+          builder: (_) => const LoginPage(),
+        );
       default:
-        // 非該当時のエラー
         throw Exception('Route not found');
     }
   }
