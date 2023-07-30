62c0431b4d21a866277560a05729793ca5bd4a4b
tombee-creator <tomomail1997@gmail.c.om>
2023-07-30 17:08:19 +0900

    リソースを追加しました。

--- lib/resource/resource.dart
+++ lib/resource/resource.dart
@@ -5,4 +5,7 @@ class Resource {
 
   String get backgroundImageUrl =>
       "https://images.unsplash.com/photo-1570025533377-7c6fffd66a7a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2534&q=80";
+
+  String get loginLabel => "LOG IN";
+  String get loginHelperMessage => "Googleアカウントのユーザ名、パスワードを入力してください。";
 }
