f4540dabb7fc437ecc297e6d3ad23bcd4619a159
tombee-creator <tomomail1997@gmail.c.om>
2023-07-30 09:04:40 +0900

    プロパティを変更しました。

--- lib/resource/resource.dart
+++ lib/resource/resource.dart
@@ -1,7 +1,7 @@
 class Resource {
-  Resource? _system;
+  Resource? _instance;
 
-  factory() => _system ??= Resource();
+  factory() => _instance ??= Resource();
 
   String get backgroundImageUrl =>
       "https://images.unsplash.com/photo-1570025533377-7c6fffd66a7a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2534&q=80";
