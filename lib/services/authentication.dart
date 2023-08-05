8c65f8701d4392bb00f328d589e2721dcc194690
tombee-creator <tomomail1997@gmail.c.om>
2023-08-05 14:31:36 +0900

    認証用クラスにメソッドを追加しました。

--- lib/services/authentication.dart
+++ lib/services/authentication.dart
@@ -20,4 +20,22 @@ class Authentication {
   }
 
   bool get isAuthenticated => FirebaseAuth.instance.currentUser != null;
+
+  Future<UserCredential> login(String email, String password) async {
+    return await FirebaseAuth.instance
+        .signInWithEmailAndPassword(email: email, password: password);
+  }
+
+  Future<UserCredential> signup(
+      String email, String password1, String password2) async {
+    if (password1 == password2) {
+      throw UnsupportedError("パスワードが一致していません");
+    }
+    return await FirebaseAuth.instance
+        .createUserWithEmailAndPassword(email: email, password: password1);
+  }
+
+  Future<void> signout() async {
+    return FirebaseAuth.instance.signOut();
+  }
 }
