f28913d3ab19aa25106e01844af97eb0282a1edd
tombee-creator <tomomail1997@gmail.c.om>
2023-08-05 19:02:39 +0900

    backgroundColorを削除

--- lib/views/home/home_page_layout.dart
+++ lib/views/home/home_page_layout.dart
@@ -24,7 +24,6 @@ class HomePageLayoutState extends State<HomePageLayout> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
       body: widget.screens[current],
