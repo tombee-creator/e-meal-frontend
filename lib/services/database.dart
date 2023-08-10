1b1ea4882f5be4e680945c15a99741c8b11661e8
tombee-creator <tomomail1997@gmail.c.om>
2023-08-10 22:31:46 +0900

    データベースマネージクラスを編集しました。

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,17 +1,17 @@
 import 'package:frontend/services/authentication.dart';
 
-import '../models/dish_model.dart';
+import '../models/recipe_model.dart';
 
 class Database {
   Database? _instance;
 
   factory() => _instance ??= Database();
 
-  Future<List<DishModel>> getDishes() async {
+  Future<List<RecipeModel>> getDishes() async {
     await Future.delayed(const Duration(seconds: 3));
     return List.generate(30, (index) => index)
         .map(
-          (index) => DishModel(
+          (index) => RecipeModel(
               Authentication().currentUser,
               "テスト${index + 1}",
               "テスト説明${index + 1}",
