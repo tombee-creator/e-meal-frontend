41e20b3a11fa9aac3940da49e6c3f8028c11738f
tombee-creator <tomomail1997@gmail.c.om>
2023-08-15 18:57:44 +0900

    テーマを変更しました。

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'つみたべ',
-      theme: ThemeData.light(),
-      darkTheme: ThemeData.dark(),
+      theme: ThemeData.dark(),
       onGenerateRoute: ApiRouter().generateRoute,
       initialRoute: '/home',
     );
