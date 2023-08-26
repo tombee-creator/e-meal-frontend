b0ee31504e8660ff5042d953e2905f32ba1acf16
tombee-creator <tomomail1997@gmail.c.om>
2023-08-26 16:52:39 +0900

    選択された食材を管理する部分を追加しました。

--- lib/views/home/components/meal-tab/meal_tab_bar_view.dart
+++ lib/views/home/components/meal-tab/meal_tab_bar_view.dart
@@ -1,3 +1,4 @@
+import 'package:emeal_app/models/ingredient.dart';
 import 'package:emeal_app/views/home/components/ingredient/ingredient_view.dart';
 import 'package:emeal_app/views/home/components/meal-preps/meal_preps_view.dart';
 import 'package:emeal_app/views/home/components/meal/meal_view.dart';
@@ -11,31 +12,43 @@ class MealTabBarView extends StatefulWidget {
 }
 
 class _MealTabBarViewState extends State<MealTabBarView> {
+  List<Ingredient> selectedIngredients = [];
+
   @override
   Widget build(BuildContext context) {
     final links = ["/ingredients", "/preps", "/meals"];
-    return Stack(
-      children: [
-        const TabBarView(children: [
-          IngredientView(),
-          MealPrepView(),
-          MealView(),
-        ]),
-        Positioned(
-            right: 20.0,
-            bottom: 20.0,
-            child: SizedBox(
+    return Stack(children: [
+      TabBarView(children: [
+        IngredientView(
+            selected: selectedIngredients,
+            onSelected: (ingredient) {
+              setState(() => selectedIngredients.add(ingredient));
+            },
+            onRemove: (ingredient) {
+              setState(() {
+                selectedIngredients = selectedIngredients
+                    .where((item) => item.id != ingredient.id)
+                    .toList();
+              });
+            }),
+        const MealPrepView(),
+        const MealView(),
+      ]),
+      Positioned(
+          right: 20.0,
+          bottom: 20.0,
+          child: SizedBox(
               width: 64,
               height: 64,
               child: FloatingActionButton(
-                child: const Icon(Icons.post_add),
-                onPressed: () {
-                  Navigator.of(context)
-                      .pushNamed(links[DefaultTabController.of(context).index]);
-                },
-              ),
-            ))
-      ],
-    );
+                  child: const Icon(Icons.post_add),
+                  onPressed: () {
+                    Navigator.of(context).pushNamed(
+                        links[DefaultTabController.of(context).index],
+                        arguments: {
+                          'ingredients': selectedIngredients,
+                        });
+                  })))
+    ]);
   }
 }
