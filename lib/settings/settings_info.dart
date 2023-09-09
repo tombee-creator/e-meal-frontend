37c560e1454f1394f64f0db7e1da0b6d32fca9e1
tombee-creator <tomomail1997@gmail.c.om>
2023-09-10 00:38:25 +0900

    AdMobを更新する処理を追加しました。

--- lib/settings/settings_info.dart
+++ lib/settings/settings_info.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/services.dart';
+import 'package:emeal_app/models/ad_info.dart';
 
 class SettingsInfo {
   static const settingsFile = "assets/settings/settings.json";
@@ -9,10 +10,12 @@ class SettingsInfo {
   factory SettingsInfo() => _instance;
 
   late final String backendUrl;
+  late final AdInfo ad;
 
   Future<void> load() async {
     final data = json.decode(await rootBundle.loadString(settingsFile));
     backendUrl = data["backendUrl"];
+    ad = AdInfo.fromJson(data);
   }
 
   SettingsInfo._internal();
